e4874776aa063a12b5d111f5f883ec675ea60ffe
Jardiland <lorenzodezen@outlook.com>
2023-04-16 15:42:03 +0100

    docstring

--- gameclass.py
+++ gameclass.py
@@ -9,7 +9,22 @@ import alias as al
 
 
 class GameClass:
+    """
+    Classe représentant un jeu.
+
+    Attributes:
+        surface (pygame.Surface): La surface du jeu.
+        surface_scale (pygame.Surface): La surface redimensionnée du jeu.
+        screen_size (tuple): La taille de la surface du jeu (largeur, hauteur).
+
+    Methods:
+        draw(surface: pygame.Surface) -> None:
+            Dessine le jeu sur la surface spécifiée.
+    """
     def __init__(self) -> None:
+        """
+        Initialise une instance de la classe GameClass.
+        """
         self.surface = pygame.Surface((1920, 1080))
         self.surface.fill((0, 0, 0, 0))
         self.surface_scale = self.surface.copy()
@@ -17,18 +32,38 @@ class GameClass:
         self.screen_size = (1920, 1080)
 
     def __draw_on_surface(self) -> None:
+        """
+        Dessine le jeu sur la surface du jeu.
 
+        Returns:
+            None
+        """
         # Draw the Game on self.surface
 
         pass
 
     def __resize(self) -> None:
+        """
+        Redimensionne la surface du jeu.
+
+        Returns:
+            None
+        """
         if self.screen_size != self.surface.get_size():
             self.surface_scale = al.im_scale(self.surface, self.screen_size)
         else:
             self.surface_scale = self.surface.copy()
 
     def draw(self, surface: pygame.Surface) -> None:
+        """
+        Dessine le jeu sur la surface spécifiée.
+
+        Args:
+            surface (pygame.Surface): La surface sur laquelle le jeu doit être dessiné.
+
+        Returns:
+            None
+        """
         self.__draw_on_surface()
         self.__resize()
         surface.blit(self.surface, (0, 0))
