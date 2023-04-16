bf334c35d9a60774296125b585daf90e8a44465d
Jardiland <lorenzodezen@outlook.com>
2023-04-16 01:53:00 +0100

    Ajout de commentaire

--- crash_catch.py
+++ crash_catch.py
@@ -22,25 +22,27 @@ def main(crash_report: str) -> None:
     Returns:
         None.
     """
+    # Définition des variables
     window = pygame.display.set_mode((300, 150))
 
-    pygame.display.set_caption("Crash Report - Lz_framework")
+    # Définition du nom de la fenêtre
+    pygame.display.set_caption("Crash Report")
 
+    # Définition de la vitesse de rafraîchissement de la fenêtre
     clock = pygame.time.Clock()
 
+    # Définition de l'affichage du curseur de la souris
     pygame.mouse.set_visible(True)
 
-    window_icon = al.im_scale(al.im_load(
-        f"file/icon/{'icon_win' if platform.system() == 'Windows' else 'icon_li'}.png"), (512, 512))
-
-    pygame.display.set_icon(window_icon)
-
+    # Création de la surface de la fenêtre
     surface = pygame.Surface(
         (window.get_width()/2, window.get_height()/2)).convert_alpha()
 
+    # Définition de la police d'écriture
     font = pygame.font.Font("file/font/KenneyMini.ttf",
                             8, bold=False, italic=False)
 
+    # Render du texte
     font_render = font.render(
         f"{window_name} a rencontré une", True, pygame.Color(255, 255, 255))
     font_render_2 = font.render(
@@ -55,6 +57,7 @@ def main(crash_report: str) -> None:
     font_render_5 = font.render(
         f"automatiquement.", True, pygame.Color(255, 255, 255))
 
+    # Affichage du texte
     surface.blit(font_render, (surface.get_width() /
                  2 - font_render.get_width()/2, 5))
     surface.blit(font_render_2, (surface.get_width() /
@@ -68,20 +71,25 @@ def main(crash_report: str) -> None:
     surface.blit(font_render_5, (surface.get_width() /
                  2 - font_render_5.get_width()/2, 55))
 
+    # Ouvre l'application de messagerie avec un nouveau message contenant un rapport d'erreur
     webbrowser.open(f"mailto:{crash_report_mail}?Subject={crash_report_subject}&body={crash_report_body.format(win_name=window_name, crash_rep=crash_report, plat_sys=platform.system(), dt_now=dt.now(), plat_ver=platform.version(), plat_arch=platform.architecture(), plat_proc=platform.processor(), plat_pyth_ver=platform.python_version())}")
 
+    # Boucle principale
     while True:
 
-        # Get every pygame event
+        # Gestion des évènements
         for event in pygame.event.get():
 
-            # Catch the quit event
+            # Gestion de la fermeture de la fenêtre
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
 
+        # Affichage du texte
         window.blit(al.im_scale(surface, (300, 150)), (0, 0))
 
+        # Rafraichissement de la fenêtre
         pygame.display.flip()
 
+        # Définition de la vitesse de rafraîchissement de la fenêtre sur 60 FPS
         clock.tick(60)
