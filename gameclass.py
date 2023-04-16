b58d95bcb99dda4b1add4e208e0969471bec8758
Jardiland <lorenzodezen@outlook.com>
2023-04-16 13:33:33 +0100

    gameclass: resize, draw

--- gameclass.py
+++ gameclass.py
@@ -4,3 +4,30 @@
 # ------------------
 
 import pygame  # Import de la libraire pygame
+
+import alias as al
+
+
+class GameClass:
+    def __init__(self) -> None:
+        self.surface = pygame.Surface((1920, 1080))
+        self.surface_scale = self.surface.copy()
+
+        self.screen_size = (1920, 1080)
+
+    def __draw_on_surface(self) -> None:
+
+        # Draw the Game on self.surface
+
+        pass
+
+    def __resize(self) -> None:
+        if self.screen_size != self.surface.get_size():
+            self.surface_scale = al.im_scale(self.surface, self.screen_size)
+        else:
+            self.surface_scale = self.surface.copy()
+
+    def draw(self, surface: pygame.Surface) -> None:
+        self.__draw_on_surface()
+        self.__resize()
+        surface.blit(self.surface, (0, 0))
