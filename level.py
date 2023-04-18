e722729598ea8186492c8309c880d7404ef2aac6
Jardiland <lorenzodezen@outlook.com>
2023-04-18 16:17:34 +0100

    added class level

--- level.py
+++ level.py
@@ -2,3 +2,9 @@
 # Author: Lorenzo De Zen / Nathan Marchand
 # License: Check the LICENSE file
 # ------------------
+
+import pygame # Import de la librairie pygame
+
+class Level:
+    def __init__(self) -> None:
+        pass
