7d7b94bad4b9e98cd719c0906392e121ca48db11
Lorenzo <lorenzodezen@outlook.com>
2023-05-15 15:20:25 +0200

    added player images

--- player.py
+++ player.py
@@ -15,3 +15,19 @@ class Player:
     def __init__(self) -> None:
         self.spritesheet = SpriteSheet(al.path(
             "files/players/texture/players.png"), al.path("files/players/data.json"))
+        
+        self.images = {
+            "green": {
+                "idle": self.spritesheet.parse_sprite(info=["green", "idle"]),
+                "push": self.spritesheet.parse_sprite(info=["green", "push"])
+            },
+            "purple": {
+                "idle": self.spritesheet.parse_sprite(info=["purple", "idle"]),
+                "push": self.spritesheet.parse_sprite(info=["purple", "push"])
+            }
+        }
+        
+        self.player_type = "green"
+        self.player_state = "idle"
+
+        self.image = self.images[self.player_type][self.player_state]
