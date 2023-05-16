fe8412bfff204515e278b8e8040bb86bfc337e44
Lorenzo <lorenzodezen@outlook.com>
2023-05-16 04:37:01 +0200

    added 10 maps

---
+++ files/maps/tilemap/tilemap.tsx
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="tilemap" tilewidth="16" tileheight="16" tilecount="132" columns="12">
+ <image source="../texture/tilemap.png" width="192" height="176"/>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collider" x="0" y="4" width="16" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collider" x="0" y="0" width="15" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collider" x="2" y="0" width="14" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="30">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collider" x="2" y="0" width="12" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collider" x="1" y="2" width="14" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collider" x="0" y="1" width="16" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="47">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collider" x="0" y="2" width="16" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="66">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collider" x="3" y="0" width="10" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="69">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collider" x="1" y="0" width="14" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="81">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collider" x="0" y="2" width="16" height="13"/>
+  </objectgroup>
+ </tile>
+</tileset>
