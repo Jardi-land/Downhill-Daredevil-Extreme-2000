63f6d12ce6edc714535538b67703126a0e279950
Lorenzo <lorenzodezen@outlook.com>
2023-05-16 13:00:29 +0200

    added map.py

---
+++ map.py
@@ -0,0 +1,11 @@
+# ------------------
+# Author: Lorenzo De Zen / Nathan Marchand
+# License: Check the LICENSE file
+# ------------------
+
+import pygame # Import de la librairie pygame
+
+
+class map:
+    def __init__(self) -> None:
+        pass
