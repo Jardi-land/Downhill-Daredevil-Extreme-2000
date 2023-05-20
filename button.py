e792ff13d933acd43ba7125bed79c43102c2c2ec
Lorenzo <lorenzodezen@outlook.com>
2023-05-21 00:55:47 +0200

    updated

--- button.py
+++ button.py
@@ -4,12 +4,14 @@
 # ------------------
 
 
-import pygame # Import pygame library
-from pygame import mixer # Import mixer from pygame library
+import pygame  # Import pygame library
+from pygame import mixer  # Import mixer from pygame library
 
 
-import alias as al # Import alias.py
-from spritesheet import SpriteSheet # Import SpriteSheet class from spritesheet.py
+import alias as al  # Import alias.py
+# Import SpriteSheet class from spritesheet.py
+from spritesheet import SpriteSheet
+
 
 def palette_swap(surf, old_color, new_color):
     """Effectue un échange de palette sur une surface donnée.
@@ -59,22 +61,23 @@ class Button:
         button = Button(text=my_text_surface, pos=(100, 200), ui_spritesheet=my_spritesheet, action="start_game")
         button.draw()
     """
+
     def __init__(self,
                  text: pygame.Surface = None,
-                 pos: tuple = (0, 0), 
-                 ui_spritesheet: SpriteSheet = None, 
+                 pos: tuple = (0, 0),
+                 ui_spritesheet: SpriteSheet = None,
                  action: str = None,
                  lock=False):
 
         from input import Input_global
         self.input_global = Input_global
-        
+
         self.text = text
-        
+
         self.lock = lock
-        
+
         self.pos = pos
-        
+
         self.scale = (480, 270)
 
         self.action = action
@@ -82,7 +85,7 @@ class Button:
         self.mouse_pos = None
 
         self.size = (self.text.get_width(), self.text.get_height())
-        
+
         mixer.init()
         self.select_sound = mixer.Sound(al.path("files/sound/select.mp3"))
         self.sound_played = False
@@ -99,22 +102,29 @@ class Button:
                                     "corner_bottom_left_click": ui_spritesheet.parse_sprite(info=["corner_bottom_left_click"]),
                                     "corner_bottom_right_click": ui_spritesheet.parse_sprite(info=["corner_bottom_right_click"]),
                                     "bottom_click": ui_spritesheet.parse_sprite(info=["bottom_click"])}
-        
-        self.surface = pygame.Surface((self.size[0] + 8, self.size[1] + 8)).convert_alpha()
+
+        self.surface = pygame.Surface(
+            (self.size[0] + 8, self.size[1] + 8)).convert_alpha()
         self.surface.fill((0, 0, 0, 0))
 
         self.surface.blit(self.button_surface_dict["corner_top_left"], (0, 0))
-        self.surface.blit(self.button_surface_dict["corner_top_right"], (self.surface.get_width() - self.button_surface_dict["corner_top_right"].get_width(), 0))
+        self.surface.blit(self.button_surface_dict["corner_top_right"], (self.surface.get_width(
+        ) - self.button_surface_dict["corner_top_right"].get_width(), 0))
         for i in range(self.surface.get_width() - 6):
             self.surface.blit(self.button_surface_dict["top"], (i + 3, 0))
-            self.surface.blit(self.button_surface_dict["bottom"], (i + 3, self.surface.get_height() - self.button_surface_dict["bottom"].get_height()))
+            self.surface.blit(self.button_surface_dict["bottom"], (
+                i + 3, self.surface.get_height() - self.button_surface_dict["bottom"].get_height()))
         for i in range(self.surface.get_height() - 8):
             self.surface.blit(self.button_surface_dict["left"], (0, i + 3))
-            self.surface.blit(self.button_surface_dict["right"], (self.surface.get_width() - self.button_surface_dict["right"].get_width(), i + 3))
-        self.surface.blit(self.button_surface_dict["corner_bottom_left"], (0, self.surface.get_height() - self.button_surface_dict["corner_bottom_left"].get_height()))
-        self.surface.blit(self.button_surface_dict["corner_bottom_right"], (self.surface.get_width() - self.button_surface_dict["corner_bottom_right"].get_width(), self.surface.get_height() - self.button_surface_dict["corner_bottom_right"].get_height()))
-        self.surface.blit(al.im_scale(self.button_surface_dict["center"], (self.size[0] + 2, self.size[1] + 2)), (3, 2))
-            
+            self.surface.blit(self.button_surface_dict["right"], (self.surface.get_width(
+            ) - self.button_surface_dict["right"].get_width(), i + 3))
+        self.surface.blit(self.button_surface_dict["corner_bottom_left"], (0, self.surface.get_height(
+        ) - self.button_surface_dict["corner_bottom_left"].get_height()))
+        self.surface.blit(self.button_surface_dict["corner_bottom_right"], (self.surface.get_width(
+        ) - self.button_surface_dict["corner_bottom_right"].get_width(), self.surface.get_height() - self.button_surface_dict["corner_bottom_right"].get_height()))
+        self.surface.blit(al.im_scale(
+            self.button_surface_dict["center"], (self.size[0] + 2, self.size[1] + 2)), (3, 2))
+
         self.mask = pygame.mask.from_surface(self.surface)
         self.mask_outline = self.mask.outline()
         self.mask_surf = self.mask.to_surface().convert_alpha()
@@ -127,10 +137,13 @@ class Button:
 
         self.surface_click = self.surface_with_mask.copy()
 
-        self.surface_click.blit(self.button_surface_dict["corner_bottom_left_click"], (0, self.surface_click.get_height() - self.button_surface_dict["corner_bottom_left_click"].get_height()))
-        self.surface_click.blit(self.button_surface_dict["corner_bottom_right_click"], (self.surface_click.get_width() - self.button_surface_dict["corner_bottom_right_click"].get_width(), self.surface_click.get_height() - self.button_surface_dict["corner_bottom_right_click"].get_height()))
+        self.surface_click.blit(self.button_surface_dict["corner_bottom_left_click"], (0, self.surface_click.get_height(
+        ) - self.button_surface_dict["corner_bottom_left_click"].get_height()))
+        self.surface_click.blit(self.button_surface_dict["corner_bottom_right_click"], (self.surface_click.get_width(
+        ) - self.button_surface_dict["corner_bottom_right_click"].get_width(), self.surface_click.get_height() - self.button_surface_dict["corner_bottom_right_click"].get_height()))
         for i in range(self.surface.get_width() - 6):
-            self.surface_click.blit(self.button_surface_dict["bottom_click"], (i + 3, self.surface.get_height() - self.button_surface_dict["bottom_click"].get_height()))
+            self.surface_click.blit(self.button_surface_dict["bottom_click"], (
+                i + 3, self.surface.get_height() - self.button_surface_dict["bottom_click"].get_height()))
 
         self.surface_click.blit(self.mask_surf, (0, 0))
 
@@ -138,16 +151,17 @@ class Button:
             self.surface.blit(self.text, (4.5, 3))
         self.surface_with_mask.blit(self.text, (4.5, 3))
         self.surface_click.blit(self.text, (4.5, 4))
-        
+
         self.grey = self.surface.copy()
         self.grey_mask = pygame.mask.from_surface(self.grey)
-        self.grey_mask_surface = self.grey_mask.to_surface(setcolor=(34, 34, 34, 140)).convert_alpha()
+        self.grey_mask_surface = self.grey_mask.to_surface(
+            setcolor=(34, 34, 34, 140)).convert_alpha()
         palette_swap(self.grey_mask_surface, (0, 0, 0), (0, 0, 0, 0))
-        
+
         self.blit_mask = False
-        
+
         self.current_surface = self.surface
-    
+
     def check_mouse(self):
         self.mouse_pos = self.input_global.get_mouse_pos()
 
@@ -157,14 +171,15 @@ class Button:
         else:
             self.blit_mask = False
             return False
-    
+
     def click_action(self):
         self.mouse = self.input_global.get_mouse_button_pressed(0)
 
         if self.mouse:
             return True
-        else: return False
-    
+        else:
+            return False
+
     def update(self):
         if self.lock:
             return self.surface
@@ -175,9 +190,9 @@ class Button:
                         self.sound_played = True
                         mixer.Sound.play(self.select_sound)
                     self.current_surface = self.surface_click
-                else: 
+                else:
                     self.current_surface = self.surface_with_mask
-            else: 
+            else:
                 self.current_surface = self.surface
 
             return self.current_surface
@@ -185,7 +200,7 @@ class Button:
     def get_action(self):
         if self.current_surface == self.surface_click:
             return self.action
-        else: 
+        else:
             return None
 
     def draw(self):
