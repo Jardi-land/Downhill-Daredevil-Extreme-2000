b5f75ed22929d6ade4c613a79698564061b96c76
Lorenzo <lorenzodezen@outlook.com>
2023-05-21 00:52:45 +0200

    updated

--- settings.py
+++ settings.py
@@ -3,10 +3,10 @@
 # License: Check the LICENSE file
 # ------------------
 
-window_name = "Downhill Daredevil Extreme 2000" # Nom de la fenêtre
+window_name = "Downhill Daredevil Extreme 2000"  # Nom de la fenêtre
 
-crash_report_mail = "nomail@yopmail.com" # Adresse mail du développeur
+crash_report_mail = "nomail@yopmail.com"  # Adresse mail du développeur
 
-crash_report_subject = f"Crash Report - {window_name}" # Sujet du mail
+crash_report_subject = f"Crash Report - {window_name}"  # Sujet du mail
 
 crash_report_body = "{win_name} a rencontré une erreur durant son exécution.\n\nVoici l'erreur rencontrée:\n{crash_rep}\n\nInformation machine:\nsystem: {plat_sys} \n time: {dt_now} \n version: {plat_ver} \n architecture:{plat_arch} \n processor:{plat_proc} \n python_v:{plat_pyth_ver}\n\nMail généré par {win_name}"
