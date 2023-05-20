4c3d6ea23c634ccdadd442c8c5ba2f58cf70b30a
Lorenzo <lorenzodezen@outlook.com>
2023-05-21 00:54:23 +0200

    updated

--- level.py
+++ level.py
@@ -5,7 +5,7 @@
 
 import pygame  # Import de la librairie pygame
 import json  # Import de la librairie json
-import random # Import de la librairie random
+import random  # Import de la librairie random
 
 import alias as al
 from player import Player
@@ -16,20 +16,20 @@ from score import Score
 
 class Level:
     def __init__(self) -> None:
-        
+
         from input import Input_global
         self.input_global = Input_global
-        
+
         self.player = Player()
-        self.camera = Camera(self.player.image, self.player.pos, self.player.rect, self.player.track)
+        self.camera = Camera(self.player.image, self.player.pos,
+                             self.player.rect, self.player.track)
         self.score = Score()
 
         self.__load_maps()
         self.__load_spawn()
         self.__init_chuck()
-        
+
         self.__camera_init()
-        
 
     def __load_maps(self):
         with open(al.path("files/maps/data.json"), "r") as f:
@@ -40,29 +40,30 @@ class Level:
         for map in self.maps_data:
             self.maps[map] = {"map": Map(
                 al.path(f"files/maps/tmx/{map}.tmx")), "weight": self.maps_data[map]["weight"]}
-            
+
     def __load_spawn(self):
         self.spawn_map = Map(al.path("files/maps/tmx/spawn.tmx"))
-        
+
     def __init_chuck(self):
         self.chunk_turn = "a"
         self.chunk_list = [self.spawn_map]
-        
+
         self.possible_chunk = []
-        
+
         for map in self.maps:
             for i in range(self.maps[map]["weight"]*3):
                 self.possible_chunk.append(map)
                 random.shuffle(self.possible_chunk)
-                    
+
         for i in range(len(self.possible_chunk)):
-            self.chunk_list.append(Map(al.path(f"files/maps/tmx/{self.possible_chunk[0]}.tmx")))
+            self.chunk_list.append(
+                Map(al.path(f"files/maps/tmx/{self.possible_chunk[0]}.tmx")))
             self.possible_chunk.pop(0)
-            
+
     def __camera_init(self):
         self.camera.spawn_map = self.spawn_map
         self.camera.calc_vector_offset()
-        
+
     def forward(self):
         self.camera.vector_offset.y -= self.player.current_speed
         self.score.score += self.player.current_speed/10
@@ -74,18 +75,17 @@ class Level:
             print(self.player.current_speed)
         self.camera.score = self.score.update()
 
-    
     def check_player_status(self):
         if self.camera.player_status == "dead":
             self.player.status = "dead"
-            
+
     def check_player_bonus(self):
         if self.camera.bonus_hit:
             print(int(self.player.current_speed/5))
             self.player.current_speed -= int(self.player.current_speed/5)
             self.camera.bonus_hit = False
             self.player.bonus_toggle()
-    
+
     def update(self):
         self.check_player_bonus()
         self.check_player_status()
