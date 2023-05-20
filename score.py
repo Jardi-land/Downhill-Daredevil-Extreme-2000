c64bf74b52bd2cd235728a23496f3e4529b84e4a
Lorenzo <lorenzodezen@outlook.com>
2023-05-21 00:52:59 +0200

    updated

--- score.py
+++ score.py
@@ -10,21 +10,21 @@ import alias as al
 from custom_font import Font
 
 
-
 class Score:
     def __init__(self) -> None:
-        self.font_spritesheet = SpriteSheet(al.path("files/font/custom/tilesheet.png"), al.path("files/font/custom/data.json"))
-        
+        self.font_spritesheet = SpriteSheet(al.path(
+            "files/font/custom/tilesheet.png"), al.path("files/font/custom/data.json"))
+
         self.font = Font(self.font_spritesheet, 6)
-        
+
         self.score = 0
         self.score_interne = 0
-        
+
         self.score_surface = self.font.render(str(self.score))
-        
+
     def render_text(self):
         self.score_surface = self.font.render(str(int(self.score)))
-        
+
     def update(self):
         self.render_text()
         return self.score_surface
